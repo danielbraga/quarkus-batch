07a27ee2e71ccd9b878012f36469cfff2dece597
Daniel Braga <daniel.souza.braga@gmail.com>
2025-05-26 11:17:06 -0300

    first commit

---
+++ src/main/java/org/acme/GreetingCommand.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Command(name = "greeting", mixinStandardHelpOptions = true)
+public class GreetingCommand implements Runnable {
+
+    @Parameters(paramLabel = "<name>", defaultValue = "picocli",
+        description = "Your name.")
+    String name;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            Locale locale = new Locale("pt", "BR");
+            DateFormat dateFormat = DateFormat.getTimeInstance(DateFormat.LONG, locale);
+            System.out.printf(String.format("%s : %s", dateFormat.format(new Date()), String.format("Hello %s, go go commando!\n", name)));
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
